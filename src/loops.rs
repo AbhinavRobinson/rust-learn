1f163f363fb166073ce7b8da6fbd3913273ca03f
AbhinavRobinson <abhinavrobinson@gmail.com>
2021-05-07 11:04:52 +0530

    (feat): add fizzbuzz with while

---
+++ src/loops.rs
@@ -0,0 +1,29 @@
+pub fn run() {
+  let mut count = 0;
+
+  // inf loop
+  loop {
+    count += 1;
+    println!("{}", count);
+
+    // stop loop
+    if count == 20 {
+      break;
+    }
+  }
+
+  count = 1;
+  // while loop (fizzbuzz)
+  while count <= 100 {
+    if count % 15 == 0 {
+      println!("FizzBuzz");
+    } else if count % 5 == 0 {
+      println!("Buzz");
+    } else if count % 3 == 0 {
+      println!("Fizz");
+    } else {
+      println!("{}", count);
+    }
+    count += 1;
+  }
+}
