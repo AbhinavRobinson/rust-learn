f2b9199a613e72ff4c0ae49c7772a57b6baa5c56
AbhinavRobinson <abhinavrobinson@gmail.com>
2021-05-07 11:06:50 +0530

    (feat): add fizzbuzz with for range

--- src/loops.rs
+++ src/loops.rs
@@ -1,29 +1,42 @@
 pub fn run() {
-  let mut count = 0;
+  // let mut count = 0;
 
-  // inf loop
-  loop {
-    count += 1;
-    println!("{}", count);
+  // // inf loop
+  // loop {
+  //   count += 1;
+  //   println!("{}", count);
 
-    // stop loop
-    if count == 20 {
-      break;
-    }
-  }
+  //   // stop loop
+  //   if count == 20 {
+  //     break;
+  //   }
+  // }
 
-  count = 1;
-  // while loop (fizzbuzz)
-  while count <= 100 {
-    if count % 15 == 0 {
+  // count = 1;
+  // // while loop (fizzbuzz)
+  // while count <= 100 {
+  //   if count % 15 == 0 {
+  //     println!("FizzBuzz");
+  //   } else if count % 5 == 0 {
+  //     println!("Buzz");
+  //   } else if count % 3 == 0 {
+  //     println!("Fizz");
+  //   } else {
+  //     println!("{}", count);
+  //   }
+  //   count += 1;
+  // }
+
+  // For Range
+  for x in 0..101 {
+    if x % 15 == 0 {
       println!("FizzBuzz");
-    } else if count % 5 == 0 {
+    } else if x % 5 == 0 {
       println!("Buzz");
-    } else if count % 3 == 0 {
+    } else if x % 3 == 0 {
       println!("Fizz");
     } else {
-      println!("{}", count);
+      println!("{}", x);
     }
-    count += 1;
   }
 }
